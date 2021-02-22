69adf71a335c87c7796f109809b52930e260d7fa
Flipza <philipp.zahner@gmx.ch>
2021-02-22 14:30:00 +0100

    Testcommit

--- protokoll.php
+++ protokoll.php
@@ -27,7 +27,7 @@ $stmt->close();
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Profile Page</title>
+		<title>Protokoll Page</title>
 		<link href="style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
