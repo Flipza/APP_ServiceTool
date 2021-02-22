fd5964a0d1b225884b1857481f1b93cc0fa317d2
Flipza <philipp.zahner@gmx.ch>
2021-02-22 16:09:12 +0100

    Protokoll Select Query test9

--- protokoll.php
+++ protokoll.php
@@ -43,7 +43,7 @@ $result1 = mysqli_query($connect, $query);
 
                 <?php while($row1 = mysqli_fetch_array($result1)):;?>
 
-                <option value="<?php echo $row1[0];?>"><?php echo $row1[1];?></option>
+                <option value="<?php echo $row1[0];?>"><?php echo $row1[0];?></option>
 
                 <?php endwhile;?>
 
