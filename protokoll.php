692dfd17f997bfa455598c7e3aa7954dddb82cb0
Flipza <philipp.zahner@gmx.ch>
2021-02-22 16:05:52 +0100

    Protokoll Select Query test8

--- protokoll.php
+++ protokoll.php
@@ -39,7 +39,7 @@ $result1 = mysqli_query($connect, $query);
 			</div>
 		</nav>
         <div>
-            <select name=serialnumbers class="dropdown">
+            <select name=serialnumbers width="300" style="width: 300px">
 
                 <?php while($row1 = mysqli_fetch_array($result1)):;?>
 
