2d437193f1e7f337b9776c84cfb3d7f8302b79cf
Flipza <philipp.zahner@gmx.ch>
2021-02-22 15:23:20 +0100

    Protokoll Dynamic Select test

--- protokoll.php
+++ protokoll.php
@@ -9,11 +9,18 @@ if (!isset($_SESSION['loggedin'])) {
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'dbuser';
 $DATABASE_PASS = 'NOIU:5678-fghj+9876';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_NAME = 'maintenance_db';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
+
+$sql = "SELECT serialnumber FROM mgp_db";
+$query = mysql_query($sql);
+while ( $results[] = mysql_fetch_object ( $query ) );
+array_pop ( $results );
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -164,7 +171,11 @@ if (mysqli_connect_errno()) {
 						<td style=min-width:50px></td>
 						<td style=min-width:50px></td>
 						<td style=min-width:50px></td>
-						<td style=min-width:50px>2688P45205</td>
+						<select name="the_name">
+    						<?php foreach ( $results as $option ) : ?>
+          					<option value="<?php echo $option->desired_value; ?>"><?php echo $option->desired_label; ?></option>
+     						<?php endforeach; ?>
+						</select>
 						<td style=min-width:50px></td>
 						<td style=min-width:50px></td>
 						<td style=min-width:50px></td>
