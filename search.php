872eca563b650ff38d2109ea1a6feecab58b9972
Flipza <philipp.zahner@gmx.ch>
2021-02-28 21:29:06 +0100

    Search Func Test 2

--- search.php
+++ search.php
@@ -2,7 +2,7 @@
     $key=$_GET['key'];
     $array = array();
     $con=mysqli_connect("localhost","dbuser","NOIU:5678-fghj+9876","maintenance_db");
-    $query=mysqli_query($con, "select * from cfg_demos where title LIKE '%{$key}%'");
+    $query=mysqli_query($con, "select * from mpg_db where serialnumber LIKE '%{$key}%'");
     while($row=mysqli_fetch_assoc($query))
     {
       $array[] = $row['title'];
