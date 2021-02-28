113cfd227620fe78119d444f2099ac359c73e430
Flipza <philipp.zahner@gmx.ch>
2021-02-28 17:07:58 +0100

    Protokoll change

--- protokoll.php
+++ protokoll.php
@@ -43,7 +43,7 @@ $query2 = "SELECT manufacturer, model, size, color, year FROM mgp_db WHERE seria
         <div>
             <form action="">
                 <label for="serialnumbers">Serial Number: </label>
-                <select id="sn_select" name=serialnumbers class="dropdown">
+                <select id="sn_select" name="serialnumbers" class="dropdown">
                     <?php while($row1 = mysqli_fetch_array($result1)):;?>
                     <option value="<?php echo $row1[0];?>"><?php echo $row1[0];?></option>
                     <?php endwhile;?>
