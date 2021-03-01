d730fbc46c8cc253acc45e44be95695d569293d4
Flipza <philipp.zahner@gmx.ch>
2021-03-01 17:34:23 +0100

    Added JQuery

--- protokoll.php
+++ protokoll.php
@@ -38,7 +38,7 @@ $result_dropdown = mysqli_query($connect, $query2);
         		//alert("You have selected the SN: " + selected_sn);
 				
 				$.get("https://www.simpli-biits.ch/db_call.php", {selected_sn: selected_sn}, function(data){
-        		var result = data;
+        		var result = array(data);
 				//var result = "test";
 				alert(result);
         		$('#man_res').val(result);
