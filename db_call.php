39ba22ed2566b5d3ab749127e37a4b8b00dc6dc5
Flipza <philipp.zahner@gmx.ch>
2021-03-01 12:20:13 +0100

    Added JQuery

--- db_call.php
+++ db_call.php
@@ -10,26 +10,22 @@ if (mysqli_connect_errno()) {
 
 // php select option value from database
 $selected_sn = ($_REQUEST['selected_sn']);
-//print_r($_REQUEST);
-//echo $selected_sn;
-//$query2 = "SELECT manufacturer FROM mgp_db WHERE serialnumber='$selected_sn'";
-$query2 = "SELECT manufacturer, model, size, color, year FROM mgp_db WHERE serialnumber='$selected_sn'";
-//echo $query2;
+$query2 = "SELECT manufacturer FROM mgp_db WHERE serialnumber='$selected_sn'";
+//$query2 = "SELECT manufacturer, model, size, color, year FROM mgp_db WHERE serialnumber='$selected_sn'";
+
 $db_id = mysqli_query($connect, $query2);
-//while ($row = mysqli_fetch_array($db_id)) {
+while ($row = mysqli_fetch_array($db_id)) {
+    $result_dropdown = $row[0];
+    //echo "guguus";
+}
+//while ($row = mysqli_fetch_array($db_id, MYSQLI_ASSOC)) {
+    //manufacturer: ".$row["manufacturer"].";
+    //model: ".$row["model"].";
+    //size: ".$row["size"].";
+    //color: ".$row["color"].";
     //$result_dropdown = $row[0];
     //echo "guguus";
 //}
-while ($row = mysqli_fetch_array($db_id, MYSQLI_ASSOC)) {
-    $result_dropdown = {
-    manufacturer: ".$row["manufacturer"].";
-    model: ".$row["model"].";
-    size: ".$row["size"].";
-    color: ".$row["color"].";
-    }
-    //$result_dropdown = $row[0];
-    //echo "guguus";
-}
 
 echo $result_dropdown;
 ?>
