1d6bbee4ad0c00f5c391ff7544f07134e7527c78
Flipza <philipp.zahner@gmx.ch>
2021-03-08 17:19:46 +0100

    Protokoll insert Function

--- insert_db.php
+++ insert_db.php
@@ -9,12 +9,12 @@ if (mysqli_connect_errno()) {
 }
 
 // php select option value from database
-$sn = $_REQUEST['sn_res'];
-$man = $_REQUEST['man_res'];
-$mod = $_REQUEST['mod_res'];
-$size = $_REQUEST['size_res'];
-$year = $_REQUEST['year_res'];
-$color = $_REQUEST['color_res'];
+$sn = $_POST['sn_res'];
+$man = $_POST['man_res'];
+$mod = $_POST['mod_res'];
+$size = $_POST['size_res'];
+$year = $_POST['year_res'];
+$color = $_POST['color_res'];
 
 $sql = "INSERT INTO mgp_dq (serialnumber, manufacturer, model, size, color, year) VALUES ("+$sn+", "+$man+", "+$mod+", "+$size+", "+$color+", "+$year+");";
 //$query2 = "SELECT manufacturer FROM mgp_db WHERE serialnumber='$selected_sn'";
@@ -26,28 +26,42 @@ if (mysqli_query($connect, $sql){
 }
 alert("Row Inserted!");
 
+if (!empty($sn) || !empty($man) || !empty($mod) || !empty($size) || !empty($year) || !empty($color)) {
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbname = "maintenance_db";
+       //create connection
+       $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+       if (mysqli_connect_error()) {
+        die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+       } else {
+        $SELECT = "SELECT serialnumber From mgp_db Where sn = ? Limit 1";
+        $INSERT = "INSERT Into mgp_db (serialnumber, manufacturer, model, size, color, year) values(?, ?, ?, ?, ?, ?)";
+        //Prepare statement
+        $stmt = $conn->prepare($SELECT);
+        $stmt->bind_param("s", $sn);
+        $stmt->execute();
+        $stmt->bind_result($sn);
+        $stmt->store_result();
+        $stmt->store_result();
+        $stmt->fetch();
+        $rnum = $stmt->num_rows;
+        if ($rnum==0) {
+         $stmt->close();
+         $stmt = $conn->prepare($INSERT);
+         $stmt->bind_param("ssssii", $sn, $man, $mod, $size, $color, $year);
+         $stmt->execute();
+         echo "New record inserted sucessfully";
+        } else {
+         echo "Paraglider already registered with this Serialnumber!";
+        }
+        $stmt->close();
+        $conn->close();
+       }
+   } else {
+    echo "All field are required";
+    die();
+   }
+   ?>
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Home Page</title>
-		<link href="style.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-	</head>
-	<body class="loggedin">
-		<nav class="navtop">
-			<div>
-				<h1>Basisrausch Servicetool</h1>
-				<a href="home.php"><i class="fas fa-user-circle"></i>Home</a>
-				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-				<a href="protokoll.php"><i class="fas fa-user-circle"></i>Prüfprotokoll</a>
-				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-			</div>
-		</nav>
-		<div class="content">
-			<h2>Home Page</h2>
-			<p>Welcome back, <?=$_SESSION['name']?>!</p>
-		</div>
-	</body>
-</html>
