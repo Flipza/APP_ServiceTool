a6c914682ada6094e858875c1ed667d9b4597f10
Flipza <philipp.zahner@gmx.ch>
2021-03-08 17:20:55 +0100

    Protokoll insert Function

--- insert_db.php
+++ insert_db.php
@@ -1,12 +1,4 @@
 <?php
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'dbuser';
-$DATABASE_PASS = 'NOIU:5678-fghj+9876';
-$DATABASE_NAME = 'maintenance_db';
-$connect = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
 
 // php select option value from database
 $sn = $_POST['sn_res'];
@@ -16,16 +8,16 @@ $size = $_POST['size_res'];
 $year = $_POST['year_res'];
 $color = $_POST['color_res'];
 
-$sql = "INSERT INTO mgp_dq (serialnumber, manufacturer, model, size, color, year) VALUES ("+$sn+", "+$man+", "+$mod+", "+$size+", "+$color+", "+$year+");";
+//$sql = "INSERT INTO mgp_dq (serialnumber, manufacturer, model, size, color, year) VALUES ("+$sn+", "+$man+", "+$mod+", "+$size+", "+$color+", "+$year+");";
 //$query2 = "SELECT manufacturer FROM mgp_db WHERE serialnumber='$selected_sn'";
 //$query_sn = "SELECT manufacturer, model, size, color, year FROM mgp_db WHERE serialnumber='$selected_sn'";
-if (mysqli_query($connect, $sql){
+/*if (mysqli_query($connect, $sql){
     echo "New Record created successfully!";
 } else {
     echo "Error";
 }
 alert("Row Inserted!");
-
+*/
 if (!empty($sn) || !empty($man) || !empty($mod) || !empty($size) || !empty($year) || !empty($color)) {
     $host = "localhost";
     $dbUsername = "root";
