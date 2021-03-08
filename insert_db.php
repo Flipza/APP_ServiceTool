d1b605eed7b575e3be02df79ad7e8c7d98db307e
Flipza <philipp.zahner@gmx.ch>
2021-03-08 19:34:47 +0100

    insertDB update

--- insert_db.php
+++ insert_db.php
@@ -1,13 +1,32 @@
 <?php
+// DB Connection
+$mysqli = new mysqli("localhost","dbuser","NOIU:5678-fghj+9876","maintenance_db");
+// Check connection
+if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+  } 
+
 
 // php select option value from database
 $sn = $_POST['sn_res'];
 $man = $_POST['man_res'];
 $mod = $_POST['mod_res'];
 $size = $_POST['size_res'];
-$year = $_POST['year_res'];
 $color = $_POST['color_res'];
+$year = $_POST['year_res'];
 
+
+// SQL Statement
+$sql = 'INSERT INTO mgp_db (serialnumber, manufacturer, model, size, color, year)';
+$sql .=" VALUES ('$sn', '$man', '$mod', '$size', '$color', '$year')";
+    
+    if ($mysqli->query($sql)){
+        echo "Records added successfully.";
+    } else{
+        echo "ERROR: Failed to execute $sql. ";
+        echo $sql;
+    }
 //$sql = "INSERT INTO mgp_dq (serialnumber, manufacturer, model, size, color, year) VALUES ("+$sn+", "+$man+", "+$mod+", "+$size+", "+$color+", "+$year+");";
 //$query2 = "SELECT manufacturer FROM mgp_db WHERE serialnumber='$selected_sn'";
 //$query_sn = "SELECT manufacturer, model, size, color, year FROM mgp_db WHERE serialnumber='$selected_sn'";
@@ -18,41 +37,5 @@ $color = $_POST['color_res'];
 }
 alert("Row Inserted!");
 */
-if (!empty($sn) || !empty($man) || !empty($mod) || !empty($size) || !empty($year) || !empty($color)) {
-    $host = "localhost";
-    $dbUsername = "dbuser";
-    $dbPassword = "NOIU:5678-fghj+9876";
-    $dbname = "maintenance_db";
-       //create connection
-       $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
-       if (mysqli_connect_error()) {
-        die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
-       } else {
-        $SELECT = "SELECT serialnumber From mgp_db Where sn = ? Limit 1";
-        $INSERT = "INSERT Into mgp_db (serialnumber, manufacturer, model, size, color, year) values(?, ?, ?, ?, ?, ?)";
-        //Prepare statement
-        $stmt = $conn->prepare($SELECT);
-        $stmt->bind_param("s", $sn);
-        $stmt->execute();
-        $stmt->bind_result($sn);
-        $stmt->store_result();
-        $stmt->store_result();
-        $stmt->fetch();
-        $rnum = $stmt->num_rows;
-        if ($rnum==0) {
-         $stmt->close();
-         $stmt = $conn->prepare($INSERT);
-         $stmt->bind_param("ssssii", $sn, $man, $mod, $size, $color, $year);
-         $stmt->execute();
-         echo "New record inserted sucessfully";
-        } else {
-         echo "Paraglider already registered with this Serialnumber!";
-        }
-        $stmt->close();
-        $conn->close();
-       }
-   } else {
-    echo "All field are required";
-    die();
-   }
+
 ?>
