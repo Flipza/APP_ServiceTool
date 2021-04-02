16efe4a12be7a824639330019476d2be549511af
Flipza <philipp.zahner@gmx.ch>
2021-04-02 10:01:04 +0200

    PHP Require test

--- protokoll.php
+++ protokoll.php
@@ -6,14 +6,7 @@ if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit;
 }
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'dbuser';
-$DATABASE_PASS = 'NOIU:5678-fghj+9876';
-$DATABASE_NAME = 'maintenance_db';
-$connect = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
+require 'mdb_creds.php';
 
 // Serialnumber SQL
 $query_sn = "SELECT serialnumber FROM mgp_db";
