11f46080dbbb5d862c656f9c9dd5aafaf4588cd6
Flipza <philipp.zahner@gmx.ch>
2021-04-02 10:15:37 +0200

    PHP Require Works

--- protokoll.php
+++ protokoll.php
@@ -29,7 +29,29 @@ $result_st = mysqli_query($connect, $query_st);
 		<link href="style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 		<script src="https://code.jquery.com/jquery-latest.js"></script>
-		<script src=”https://www.simpli-biits.ch/script.js”></script>
+		<script>
+			// Serialnumber Dropdownmenü
+			$(document).ready(function(){
+    			$("select.dropdown_sn").change(function(){
+    			var selected_sn = $(this).children("option:selected").val();
+    			$.getJSON("https://www.simpli-biits.ch/db_call.php", {selected_sn: selected_sn}, function(data){
+        			$('#sn_res').val(data['serialnumber']);
+        			$('#man_res').val(data['manufacturer']);
+        			$('#mod_res').val(data['model']);
+        			$('#size_res').val(data['size']);
+        			$('#color_res').val(data['color']);
+        			$('#year_res').val(data['year']);
+        			// MGP General Abschnitt ReadOnly
+        			$('#sn_res_ro').val(data['serialnumber']);
+        			$('#man_res_ro').val(data['manufacturer']);
+        			$('#mod_res_ro').val(data['model']);
+        			$('#size_res_ro').val(data['size']);
+        			$('#color_res_ro').val(data['color']);
+        			$('#year_res_ro').val(data['year']);
+        			});
+    			});
+			});
+		</script>
 	</head>
 	<body class="loggedin">
 		<nav class="navtop">
